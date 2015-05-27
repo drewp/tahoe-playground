6c718251a8a417389cf6133bc3fd25e002d77f2e
Drew Perttula <drewp@bigasterisk.com>
2015-05-26 23:15:10 -0700

    port from prototype to jquery. add license file
    Ignore-this: 8aa49babac7920074cf6729bfcfb8a01
    
    darcs-hash:20150527061510-836b1-af65902dfca6416f84d33341618d59f3ac1114d7

--- www/sim.js
+++ www/sim.js
@@ -1,49 +1,47 @@
 var sim = {};
 
 (function() {
-var alphabet = "abcdefghijklmnopqrstuvwxyz234567";
+var alphabet = 'abcdefghijklmnopqrstuvwxyz234567';
 var N_shares = 10;
 var K_required = 3;
 var encryptionRelativeSize = 3.0;
 
 sim.onEncrypt = function() {
-    var cleartext = $('cleartext').value;
+    var cleartext = $('.cleartext').val();
     var fakeHash = simpleHash(cleartext);
 
-    $('url').update("http://localhost:8123/uri/URI%3ACHK%3A" + 
-		    randomHex(fakeHash + 1234, 128) + ":" + 
-		    randomHex(fakeHash + 5678, 256) + ":" + 
-		    K_required + ":" + N_shares + ":" + cleartext.length);
-
-    $('encrypted').value = randomHex(fakeHash + 9876, 
-				     cleartext.length * 8 * encryptionRelativeSize, 
-				     100);
+    $('#url').text('http://localhost:8123/uri/URI%3ACHK%3A' + 
+		   randomHex(fakeHash + 1234, 128) + ':' + 
+		   randomHex(fakeHash + 5678, 256) + ':' + 
+		   K_required + ':' + N_shares + ':' + cleartext.length);
+    
+    $('#encrypted').val(randomHex(fakeHash + 9876, 
+				  cleartext.length * 8 * encryptionRelativeSize, 
+				  100));
     sim.updateActiveSteps();
 }
 
 sim.onStore = function() {
-    var cleartext = $('cleartext').value;
+    var cleartext = $('#cleartext').val();
     var fakeHash = simpleHash(cleartext);
-    $('shares').update('');
+    $('#shares').empty();
     for (var row=0; row < Math.ceil(N_shares / 4); row++) {
-	var rowElem = new Element('tr');
+	var rowElem = $('<tr>');
 	for (var col=0; col < 4 && (row * 4 + col < N_shares); col++) {
 	    var shareNum = row*4+col + 1;
-	    var td = new Element('td');
+	    var td = $('<td>');
 
-	    td.appendChild(new Element('div').update(
-		'#' + shareNum));
+	    td.append($('<div>').text('#' + shareNum));
 	    var data = randomHex(fakeHash + shareNum, 
 		                 cleartext.length * 8 / K_required, 25);
-	    td.appendChild(new Element('textarea', 
-				       {class: 'encrypted share', 
-					id: 'share'+shareNum,
-					correctValue: data,
-					cols: '26', rows: '4'}).update(data));
+	    td.append($('<textarea>').attr({class: 'encrypted share', 
+					    id: 'share'+shareNum,
+					    correctValue: data,
+					    cols: '26', rows: '4'}).val(data));
 
-	    rowElem.appendChild(td);
+	    rowElem.append(td);
 	}
-	$('shares').appendChild(rowElem);
+	$('#shares').append(rowElem);
     }
     sim.updateActiveSteps();
 }
@@ -51,44 +49,41 @@ sim.onStore = function() {
 sim.onRecover = function() {
     var usableShares = new Array();
     for (var i=1; i <= N_shares; i++) {
-	var cell = $('share' + i);
+	var cell = $('#share' + i);
 
-	cell.removeClassName('share-used');
-	cell.removeClassName('share-errored');
+	cell.removeClass('share-used share-errored');
 
 	if (usableShares.length >= K_required) {
 	    continue;
 	}
 
-	if (cell.value == cell.getAttribute('correctValue')) {
+	if (cell.val() == cell.attr('correctValue')) {
 	    usableShares.push(i);
-	    cell.addClassName('share-used');
+	    cell.addClass('share-used');
 	} else {
-	    cell.addClassName('share-errored');
+	    cell.addClass('share-errored');
 	}
     }
     if (usableShares.length == K_required) {
-	$('recovered').value = $('cleartext').value; // sssh!
-	$('recover-report').update("Recovery used these shares: " + usableShares);
+	$('#recovered').val($('#cleartext').val()); // sssh!
+	$('#recover-report').text('Recovery used these shares: ' + usableShares);
     } else {
-	$('recovered').value = "";
-	$('recover-report').update("Recovery failed");
+	$('#recovered').val('');
+	$('#recover-report').text('Recovery failed');
     }
 }
 
 sim.updateActiveSteps = function() {
-    $('active-encrypt').addClassName('disabled');
-    $('active-store').addClassName('disabled');
-    $('active-recover').addClassName('disabled');
+    $('#active-encrypt, #active-store, #active-recover').addClass('disabled');
 
-    if ($('cleartext').value != '') {
-	$('active-encrypt').removeClassName('disabled');
+    if ($('#cleartext').val() != '') {
+	$('#active-encrypt').removeClass('disabled');
 
-	if ($('encrypted').value != '') {
-	    $('active-store').removeClassName('disabled');
+	if ($('#encrypted').val() != '') {
+	    $('#active-store').removeClass('disabled');
 
-	    if ($('shares').descendants().length > 3) {
-		$('active-recover').removeClassName('disabled');
+	    if ($('#shares').find('td').length > 3) {
+		$('#active-recover').removeClass('disabled');
 	    }
 	}
     }
@@ -104,13 +99,13 @@ function awfulRand() {
 }
 
 function randomHex(seed, bits, width) {
-    var result = "";
+    var result = '';
     awfulRandSeed(seed);
     var chars = charsNeeded(bits, alphabet.length);
     for (var i=0; i < chars; i++) {
 	result += alphabet[Math.floor(awfulRand() * alphabet.length)];
 	if (!(width == undefined) && (i + 1) % width == 0) {
-	    result += "\n";
+	    result += '\n';
 	}
     }
     return result;
